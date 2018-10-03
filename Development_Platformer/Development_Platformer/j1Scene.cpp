71a578ad9236f54afd2ecc975f924ffcc94132c9
vsRushy <vsRushy@users.noreply.github.com>
2018-10-03 19:20:21 +0200

    Screen title is not the map info anymore
    Just uncomment to see the information.

--- Development_Platformer/Development_Platformer/j1Scene.cpp
+++ Development_Platformer/Development_Platformer/j1Scene.cpp
@@ -64,14 +64,14 @@ bool j1Scene::Update(float dt)
 	//App->render->Blit(img, 0, 0);
 	App->map->Draw();
 
-	// TODO 7: Set the window title like
-	// "Map:%dx%d Tiles:%dx%d Tilesets:%d"
-	p2SString title("Map:%dx%d Tiles:%dx%d Tilesets:%d",
+	// Set the window title like
+	// "Map:%dx%d Tiles:%dx%d Tilesets:%d"                            // Uncomment the following if you want to see tileset info as window title
+	/*p2SString title("Map:%dx%d Tiles:%dx%d Tilesets:%d",
 		App->map->data.width, App->map->data.height,
 		App->map->data.tile_width, App->map->data.tile_height,
 		App->map->data.tilesets.count());
 
-	App->win->SetTitle(title.GetString());
+	App->win->SetTitle(title.GetString());*/
 	return true;
 }
 
