627faab6b4da9d902c43b75de27e4d903ed16d97
vsRushy <gerardmarcosfreixasgmail.com>
2018-10-06 13:36:31 +0200

    Defined player very basic skeleton on the .cpp

--- Development_Platformer/Development_Platformer/j1Player.cpp
+++ Development_Platformer/Development_Platformer/j1Player.cpp
@@ -1,5 +1,6 @@
 #include "j1App.h"
 #include "p2Defs.h"
+#include "p2Log.h"
 #include "j1Textures.h"
 #include "j1Render.h"
 #include "j1Input.h"
@@ -7,3 +8,54 @@
 #include "j1Player.h"
 #include "j1Collision.h"
 
+j1Player::j1Player()
+{
+
+}
+
+j1Player::~j1Player()
+{}
+
+bool j1Player::Awake(pugi::xml_node& data)
+{
+	return true;
+}
+
+// Load assets
+bool j1Player::Start()
+{
+	LOG("Loading player textures");
+
+	LOG("Loading player sound effects");
+
+	return true;
+}
+
+bool j1Player::CleanUp()
+{
+	LOG("Unloading player");
+
+	// Delete collider
+
+	return true;
+}
+
+bool j1Player::Update(float dt)
+{
+	return true;
+}
+
+bool j1Player::Load(pugi::xml_node& data)
+{
+	return true;
+}
+
+bool j1Player::Save(pugi::xml_node& data) const
+{
+	return true;
+}
+
+void j1Player::OnCollision(Collider* a, Collider* b)
+{
+
+}
