cc14835c8a4a258142f27a287b64eb131996778e
vsRushy <vsRushy@users.noreply.github.com>
2018-11-09 01:50:38 +0100

    Adding pathfinding code ...

--- Development_Platformer/Development_Platformer/Enemy_level01_air.cpp
+++ Development_Platformer/Development_Platformer/Enemy_level01_air.cpp
@@ -2,11 +2,14 @@
 #include "Enemy_level01_air.h"
 #include "j1Collision.h"
 #include "ModuleEnemies.h"
+#include "j1Map.h"
+
+#define AIR_SIZE_01 32
 
 Enemy_level01_air::Enemy_level01_air(int x, int y) : Enemy(x, y)
 {
 
-	anim.PushBack({ 64, 82, 32, 32 });
+	anim.PushBack({ 64, 82, AIR_SIZE_01, AIR_SIZE_01 });
 	anim.loop = true;
 	anim.speed = 0.5f;
 
@@ -14,14 +17,64 @@ Enemy_level01_air::Enemy_level01_air(int x, int y) : Enemy(x, y)
 
 	animation = &anim;
 
-	collider = App->collision->AddCollider({ 0, 0, 32, 32 }, COLLIDER_TYPE::COLLIDER_ENEMY, (j1Module*)App->enemies);
+	collider = App->collision->AddCollider({ 0, 0, AIR_SIZE_01, AIR_SIZE_01 }, COLLIDER_TYPE::COLLIDER_ENEMY, (j1Module*)App->enemies);
 
-	original_x = x;
-	original_y = y;
+	original_pos.x = x;
+	original_pos.y = y;
 }
 
 void Enemy_level01_air::Move()
 {
 	position.x = position.x;
 	position.y = position.y;
+}
+
+void Enemy_level01_air::Update(float dt)
+{
+	first_point = App->map->WorldToMap(position.x, position.y);
+	last_point = App->map->WorldToMap(position.x + AIR_SIZE_01, position.y + AIR_SIZE_01);
+}
+
+void Enemy_level01_air::CreateRange()
+{
+	uint counter = 0;
+
+	for (int i = -3; i < 4; i++)
+		for (int k = -3; k < 4; k++)
+		{
+			iPoint tmp_range = App->map->WorldToMap(position.x, position.y);
+			if (!(i == 0 && k == 0)) {
+				tmp_range.x += i;
+				tmp_range.y += k;
+				range[counter++] = tmp_range;
+			}
+		}
+}
+
+void Enemy_level01_air::PathMovement(const p2DynArray<iPoint>* path, iPoint position, float speed)
+{
+	iPoint goal = iPoint(path->At(0)->x, path->At(0)->y); // we want the initial point of the path!
+	float velocity_x = 0.0f;
+	float velocity_y = 0.0f;
+
+	if(goal.x < position.x)
+	{ 
+		velocity_x = -5.0f;
+	}
+	else if (goal.x > position.x)
+	{
+		velocity_x = 5.0f;
+	}
+	else if (goal.y < position.y)
+	{
+		velocity_y = -5.0f;
+	}
+	else if (goal.y > position.y)
+	{
+		velocity_y = 5.0f;
+	}
+
+	is_moving = true;
+	velocity.x = velocity_x;
+	velocity.y = velocity_y;
 }
--- Development_Platformer/Development_Platformer/Enemy_level01_air.h
+++ Development_Platformer/Development_Platformer/Enemy_level01_air.h
@@ -3,17 +3,33 @@
 
 #include "Enemy.h"
 
+#include "p2Point.h"
+#include "p2DynArray.h"
+
 class Enemy_level01_air : public Enemy
 {
 private:
-	int original_x = 0;
-	int original_y = 0;
+	iPoint original_pos;
 	Animation anim;
+
+	bool is_moving = false;
+	bool player_is_range = false;
+	
+	iPoint objective;
+	iPoint range[32];
+
+	iPoint velocity;
+
+	iPoint first_point;
+	iPoint last_point;
 
 public:
 	Enemy_level01_air(int x, int y);
 
 	void Move();
+	void Update(float dt);
+
+	void PathMovement(const p2DynArray<iPoint>*, iPoint, float);
 };
 
 #endif // __Enemy_level01_air__
